64b7ddb6e7c218769637effcfe35e121700aa5fe
github-user865436 <132678398+github-user865436@users.noreply.github.com>
2025-01-30 08:35:13 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,16 @@
-
+local Triples = {}
+for C = 3, 99 do
+  for A = 1, C - 1 do
+    for B = 1, A - 1 do
+      local Dupe = false
+      for K = 2, C - 1 do
+        if math.floor(A / K) == A / K and math.floor(B / K) == B / K and math.floor(C / K) == C / K then
+          Dupe = true
+        end
+        if A*A + B*B == C*C and not Dupe then
+          table.insert(Triples, {A, B, C})
+        end
+      end
+    end
+  end
+end
